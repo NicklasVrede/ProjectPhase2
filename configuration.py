ab727ae7d69fa43151c226573362db491ca929d7
Nicklas Vrede <32699906+NicklasVrede@users.noreply.github.com>
2023-12-13 20:28:30 +0100

    New modules

---
+++ configuration.py
@@ -0,0 +1,235 @@
+"""
+Config Module
+"""
+import random
+
+line =  "\n"*3 + "-" * 40 + "\n"*3
+menu_strings = {
+    "gen_method" : line + """
+Please pick a generation method of the graph
+    """,
+
+    "ini_land_pattern" : line + """
+Choose an option for the initial landscape pattern:
+
+1. All woods
+
+2. All rocks
+
+3. Random (e.g. 80 % wood)
+    """,
+
+    "firefigter_num" : line + """
+Choose the number of firefighter.
+    """,
+
+    "firefigter_level" : line + """
+Choose the average skill level firefighters:
+
+1. Low
+
+2. Medium
+
+3. High
+    """,
+
+    "iter_num" : line + """
+How many iteration steps would you like?
+    """,
+
+    "change_setting" : line + """
+Which option would you like to change?
+
+1. Generation Method
+
+2. Initial landscape pattern
+
+3. Firefighter number
+
+4. Firefighter skill level
+
+5. Iteration steps
+
+6. All
+    """
+    
+    }
+
+def gen_method(options:dict=dict()):
+    print(menu_strings.get("gen_method"))
+    while True:
+        user_input = input('Enter "1", "2", "3" or press back: ')
+
+        if user_input == "1":
+            choice = "read"
+            break
+
+        elif user_input == "2":
+            choice = "standard"
+            break
+
+        elif user_input == "3":
+            choice = "random"
+            break
+
+        else:
+            print("Wrong input, please try agian")
+
+    options.update({"gen_method" : choice})
+    
+    if len(options) == 5:
+        return main(options)
+    else:
+        return ini_land_pattern(options)
+
+
+def ini_land_pattern(options:dict):
+    print(menu_strings.get("ini_land_pattern"))
+
+    while True:
+        user_input = input('Enter "1", "2", "3": ')
+        if user_input == "1":
+            choice = "wood"
+            break
+
+        elif user_input == "2":
+            choice = "rock"
+            break
+
+        elif user_input == "3":
+            choice = "random"
+            break
+
+        else:
+            print("Wrong input, please try agian")
+
+    options.update({"ini_land_pattern" : choice})
+
+    if len(options) == 5:
+        return main(options)
+    else:
+        return firefighter_num(options)
+
+def firefighter_num(options:dict):
+    print(menu_strings.get("firefigter_num"))
+
+    while True:
+        user_input = input('Enter a number or "r" for random: ')
+        if user_input == "r":
+            choice = "r"
+            break
+
+        try:
+            choice = int(user_input)
+            break
+
+        except ValueError("Enter a whole number"):
+            print("Enter a whole number")
+
+    
+    options.update({"firefigter_num" : choice})
+
+    if len(options) == 5:
+        return main(options)
+    else:
+        return firefighter_level(options)
+
+def firefighter_level(options:dict):
+    print(menu_strings.get("firefigter_level"))
+
+    while True:
+        user_input = input('Enter a "1", "2", "3", or "r" for random: ')
+        if user_input == "r":
+            choice = "r"
+            break
+
+        try:
+            choice = int(user_input)
+            break
+
+        except ValueError("Enter a whole number"):
+            print("Enter a whole number")
+
+    print(choice)
+    options.update({"firefigter_level" : choice})
+
+    if len(options) == 5:
+        return main(options)
+    else:
+        return iter_num(options)
+
+def iter_num(options:dict):
+    print(menu_strings.get("iter_num"))
+
+    while True:
+        user_input = input('Enter a number, or "r" for random: ')
+        if user_input == "r":
+            choice = "r"
+            break
+
+        try:
+            choice = int(user_input)
+            break
+
+        except ValueError("Enter a whole number"):
+            print("Enter a whole number")
+            
+    options.update({"iter_num" : choice})
+
+    return main(options)
+
+def change_setting(options):
+    print(menu_strings.get("change_setting"))
+
+    while True:
+        user_input = input('Enter "1", "2", "3", "4" or "5":')
+        if user_input == "1":
+            return gen_method(options)
+
+        elif user_input == "2":
+            return ini_land_pattern(options)
+
+        elif user_input == "3":
+            return firefighter_num(options)
+            break
+
+        elif user_input == "4":
+            return firefighter_level(options)
+            break
+
+        elif user_input == "5":
+            return iter_num(options)
+            break
+
+        else:
+            print("Wrong input, please try agian")
+      
+def main(options:dict=dict()):
+    print(line + f"""
+Current options:
+
+Generation Method: {options.get("gen_method")}
+Initial landscape: {options.get("ini_land_pattern")}
+Firefighter number: {options.get("firefigter_num")}
+Firefighter skill level: {options.get("firefigter_level")}
+Iteration steps: {options.get("iter_num")}
+
+How would you like to proceed?
+
+1. Start simulation.
+
+2. Change a setting.
+""")
+    
+    while True:
+        user_input = input('Enter "1" or "2": ')
+
+        if user_input == "1":
+            print("placeholder")
+            break
+
+        elif user_input == "2":
+            return change_setting(options)
+   
+
+gen_method()
