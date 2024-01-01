c9e8a945a5ca550cd15726242bbb422044400110
Nicklas Vrede <32699906+NicklasVrede@users.noreply.github.com>
2024-01-01 08:57:48 +0100

    serperate folder! :D

--- tests/test_firefighter.py
+++ tests/test_firefighter.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 from firefighter import Firefighter
 from land_rep import TreePatch, RockPatch
