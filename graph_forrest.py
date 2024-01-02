9f47317c9b30a40d280464eee78dfcb0ad29946d
Nicklas Vrede <32699906+NicklasVrede@users.noreply.github.com>
2024-01-02 18:25:21 +0100

    q

--- graph_forrest.py
+++ graph_forrest.py
@@ -36,7 +36,6 @@ def main(options: Dict[str, int] = dict()) -> None:
     
     #2. Generate edges and positions - Initialiser.py:
     edges, positions = generate_edges(options)
-    print(f'Positions: {positions}')
 
     #3. Initialise patch objects - Initialiser.py:
     patches = initialise_patches(edges, positions, options)
